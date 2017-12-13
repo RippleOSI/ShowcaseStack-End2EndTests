6c169f9683444126ba112487b204b27e7f66ca37
s-kim <sergejjkim@gmail.com>
2017-12-13 23:30:33 +0200

    Version Switch

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ For Windows
 * Set path to selenium-standalone-server.jar and chromedriver.exe in nightwatch.json selenium > server_path
 * Set path to chromedriver.exe in nightwatch.json selenium > cli_args > webdriver.chrome.driver
 * Run tests from parent catalog command line with command 'nightwatch'
+
+# nightwatch.json
+Is a config file. See for full details http://nightwatchjs.org/gettingstarted/#settings-file
+The important part besides paths to chrome driver and selenium server is the ability to set several environments.
+Environments contain information about your app url, selenium server url (which shouldn't be changed unless you are 
+going to use remote servers like selenium grid or cloud solutions), whether to do screenshots and where to put those, etc.
+Also the switch between versions of app (react/angular) is implemented with environments. There is a default env that
+is pointing to https://secureshowcase.ripple.foundation/ with no version switch and dev_react and dev_angular 
+each pointing to https://securedev.ripple.foundation/ with switching to corresponding version.
+You can easily switch the env you want to run tests on by adding --env env_name to your nightwatch command, i.e. 
+nightwatch --dev_react
+Version switch is implemented with API calls, see globals.js. So if you want to add a new env with switching versions, 
+make sure to set
+* "version_switch_host": "dev.ripple.foundation", - host of API calls
+* "version_switch_init_path": "/api/initialise", - to provide authorization
+* "version_switch_path": "/api/ui/version_name" 
---
+++ globals.js
@@ -0,0 +1,53 @@
+module.exports = {
+
+    /**
+     * Switches to specific version of app. To work properly, the env has to have following properties:
+     *  version_switch_host
+     *  version_switch_path
+     *  version_switch_init_path
+     * If those are missing, the switch won't work. Env is the part of nightwatch.json test_settings object
+     *
+     * @param browser
+     * @param done
+     */
+    beforeEach: function (browser, done) {
+        if (browser.globals.version_switch_host !== "") {
+            console.log("Switching to " + browser.globals.version_switch_path);
+
+            var http = require('http');
+
+            function printStatus(response) {
+                console.log("Got response for version switch: " + response.statusCode);
+            }
+
+            function initCallBack(response) {
+                console.log("Got response for init: " + response.statusCode);
+
+                response.on("data", function (chunk) {
+                    var text = "" + chunk;
+                    var token = text.split(",")[0].split(":")[1].replace(/[{"]/g, "");
+                    console.log("Auth token: " + token);
+                    var options = {
+                        host: browser.globals.version_switch_host,
+                        path: browser.globals.version_switch_path,
+                        headers: {
+                            Authorization: token
+                        }
+                    };
+                    http.get(options, printStatus);
+                });
+            }
+
+            var options = {
+                host: browser.globals.version_switch_host,
+                path: browser.globals.version_switch_init_path
+            };
+
+            http.get(options, initCallBack);
+        } else {
+            console.log("No version switch, if that is not expected, check nightwatch.json for preferences.")
+        }
+        console.log("Starting");
+        done();
+    }
+};
