4435362e87a54fcede3320a6bf387ca099a48004
s-kim <sergejjkim@gmail.com>
2017-12-25 16:49:41 +0200

    date picker2

---
+++ page_objects/angularDatePicker.js
@@ -0,0 +1,7 @@
+module.exports = {
+    elements: {
+        buttonBack: ".uib-daypicker .pull-left",
+        buttonForward: ".uib-daypicker .pull-right",
+        currentMonth: ".uib-daypicker .uib-title"
+    }
+};
---
+++ page_objects/reactDatePicker.js
@@ -0,0 +1,7 @@
+module.exports = {
+    elements: {
+        buttonBack: ".react-datepicker__navigation--previous",
+        buttonForward: ".react-datepicker__navigation--next",
+        currentMonth: ".react-datepicker__current-month"
+    }
+};
